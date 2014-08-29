e0153bd6db92cb4c884d3d6771b80cbacd7db1ce
Rahool <rahooolpatil.rp@gmail.com>
2014-08-29 19:29:48 +0530

    New Fragment is created and displayed on every Tab
    new Packages are created
    1. one for network stuff
    2. one for Fragment here all fragment classes are to be stored

---
+++ GCMProject/src/com/example/fragment/GCMRegisterFragment.java
@@ -0,0 +1,137 @@
+/**
+ * @author rahool
+ * date: 29 August 2014 
+ *
+ */
+package com.example.fragment;
+
+import com.example.gcmproject.R;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass. Activities that
+ * contain this fragment must implement the
+ * {@link GCMRegisterFragment.OnFragmentInteractionListener} interface to handle
+ * interaction events. Use the {@link GCMRegisterFragment#newInstance} factory
+ * method to create an instance of this fragment.
+ * 
+ */
+public class GCMRegisterFragment extends Fragment {
+
+	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+	private static final String ARG_PARAM1 = "param1";
+	private static final String ARG_PARAM2 = "param2";
+
+	private String mParam1;
+	private String mParam2;
+
+	private String regid;
+	private String PROJECT_NUMBER = "184169187173";  //here replace with your own GCMProject key 
+	
+	Button register_btn;
+	TextView registration_key_tv;
+	
+	private OnFragmentInteractionListener mListener;
+
+	/**
+	 * Use this factory method to create a new instance of this fragment using
+	 * the provided parameters.
+	 * 
+	 * @param param1
+	 *            Parameter 1.
+	 * @param param2
+	 *            Parameter 2.
+	 * @return A new instance of fragment GCMRegisterFragment.
+	 */
+
+	public static GCMRegisterFragment newInstance(String param1, String param2) {
+		GCMRegisterFragment fragment = new GCMRegisterFragment();
+		Bundle args = new Bundle();
+		args.putString(ARG_PARAM1, param1);
+		args.putString(ARG_PARAM2, param2);
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	public GCMRegisterFragment() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (getArguments() != null) {
+			mParam1 = getArguments().getString(ARG_PARAM1);
+			mParam2 = getArguments().getString(ARG_PARAM2);
+		}
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View v = inflater
+				.inflate(R.layout.fragment_gcmregister, container, false);
+		
+		register_btn = (Button) v.findViewById(R.id.button_register_with_gcm);
+		registration_key_tv = (TextView) v.findViewById(R.id.textview_press_the_button_to_get_your_gcm_register_id);
+		
+		register_btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				
+			}
+		});
+		return v;
+	}
+
+	// TODO: Rename method, update argument and hook method into UI event
+	public void onButtonPressed(Uri uri) {
+		if (mListener != null) {
+			mListener.onFragmentInteraction(uri);
+		}
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		try {
+			mListener = (OnFragmentInteractionListener) activity;
+		} catch (ClassCastException e) {
+			throw new ClassCastException(activity.toString()
+					+ " must implement OnFragmentInteractionListener");
+		}
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		mListener = null;
+	}
+
+	/**
+	 * This interface must be implemented by activities that contain this
+	 * fragment to allow an interaction in this fragment to be communicated to
+	 * the activity and potentially other fragments contained in that activity.
+	 * <p>
+	 * See the Android Training lesson <a href=
+	 * "http://developer.android.com/training/basics/fragments/communicating.html"
+	 * >Communicating with Other Fragments</a> for more information.
+	 */
+	public interface OnFragmentInteractionListener {
+		// TODO: Update argument type and name
+		public void onFragmentInteraction(Uri uri);
+	}
+
+}
---
+++ GCMProject/src/com/example/network/MyTask.java
@@ -0,0 +1,21 @@
+package com.example.network;
+
+import android.os.AsyncTask;
+
+public class MyTask extends AsyncTask<Void, Void, String> {
+
+	@Override
+	protected String doInBackground(Void... params) {
+		
+		String msg = "";
+			
+		return null;
+	}
+	
+	@Override
+	protected void onPostExecute(String result) {
+
+		super.onPostExecute(result);
+	}
+
+}
